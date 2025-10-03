219dff18ca6181c1ee1a96d5903989ff20bef0a0
RaphaelBFagundes <raphael.bf1@puccampinas.edu.br>
2025-10-03 17:54:22 -0300

    atualização teste.c

--- teste.c
+++ teste.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <PILHAEFILA.h>
+
+typedef struct data{
+    int dia;
+    int mes;
+    int ano;
+}data;
+
+typedef struct pet{
+    int id;
+    char nome[50];
+    char epecie[30];
+    int idade;
+    int prioridade;
+    data nasc;
+    pet *prox;
+}pet;
+
+
+void inserirPet(pet *p, fila *fila0, fila *fila1, char *nome, char *epecie, int id, int idade, int prioridade){
+
+
+}
+
+void atenderPet(fila *fila0, fila *fila1, pet *p){
+    
+}
+
 int main() {
-    printf("Hello world\n");
+    srand(time(NULL));
+    fila* fila0 = criafila();
+    fila* fila1 = criafila();
+
     return 0;
 }
