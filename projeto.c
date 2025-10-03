aca027a87f894a35b70d2572b388c45faff83236
RaphaelBFagundes <raphael.bf1@puccampinas.edu.br>
2025-10-03 17:49:53 -0300

    atualização teste.c

---
+++ projeto.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <PILHAEFILA.h>
+#include <time.h>
+
+
+typedef struct data{
+    int dia, mes, ano;
+} data;
+
+typedef struct pet{
+    int id;                
+    char nome[51];          
+    char especie[31];       
+    int idade;
+    data nasc;
+    int prioridade;         
+    int atendido;          
+} pet;
+typedef struct filapet{
+    pet *inicio;
+    pet *fim;
+}filapet;
+filapet* criafilapet ()
+{
+    filapet* f = malloc(sizeof(filapet));
+    f->inicio = f->fim = NULL;
+    return f;
+}
+void inserepet(filapet *f)
+{
+    pet *novo = malloc(sizeof(pet));
+    novo -> id = rand() % 999 + 1;
+    printf("digite o nome:");
+    fgets(novo->nome, sizeof(novo->nome), stdin);
+    printf("digite a especie");
+    fgets(novo->especie, sizeof(novo->especie), stdin);
+    printf("digite a idade:");
+    scanf("%d",&novo->idade);
+
+
+
+    
+    
+   
+}
